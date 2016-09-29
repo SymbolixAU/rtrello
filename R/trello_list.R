88716ae73c4bd03ad64741d0e78873d51027ca34
David Cooley <david@leonardo.local>
2016-09-29 11:11:01 +1000

    added card_status to trello_list

--- R/trello_list.R
+++ R/trello_list.R
@@ -5,10 +5,10 @@
 #' @export
 #' @param list_id string list id
 #' @param trello trello object containing your key and token
-trello_list <- function(list_id, trello){
+trello_list <- function(list_id, trello, card_status = c("all", "open", "closed", "none")){
 
 	jsonlite::fromJSON(paste0("https://api.trello.com/1/lists/", list_id,
-														"?fields=name&cards=all&card_fields=all&key=", trello@key,
+														"?fields=name&cards=", card_status, "&card_fields=all&key=", trello@key,
 														"&token=", trello@token))
 
 }
